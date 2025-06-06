8d70bab045e42bcd48ffe4e6f131fb70779f7f7c
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-06-06 23:08:26 +0500

    list.js

--- api/v1/list.js
+++ api/v1/list.js
@@ -1,30 +1,30 @@
 export default function handler(req, res) {
   const endpoints = [
     {
-      path: "/api/time",
+      path: "/api/v1/time",
       description: "Returns the current server time."
     },
     {
-      path: "/api/encode",
+      path: "/api/v1/encode",
       description: "Encodes provided content into Base64."
     },
     {
-      path: "/api/color",
+      path: "/api/v1/color",
       description: "Get any color information, custom hex color code can be input."
     },
     {
-      path: "/api/emoji",
+      path: "/api/v1/emoji",
       description: "Get specified or random emoji info."
     },
     {
-      path: "/api/list",
+      path: "/api/v1/list",
       description: "Retrieves a list of all available endpoints."
     }
   ];
 
   res.json({
     message: "All Available API endpoints, made by harys722.",
-    base_url: "https://harys.is-a.dev",
+    base_url: "https://harys.is-a.dev/api/v1",
     endpoints: endpoints
   });
 }
