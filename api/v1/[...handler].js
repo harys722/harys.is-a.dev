523578e07f506e47925d49aff1801e4cb6bf68f8
- HARYs - <174037813+harys722@users.noreply.github.com>
2025-06-11 20:20:08 +0500

    [...handler].js

--- api/v1/[...handler].js
+++ api/v1/[...handler].js
@@ -10,12 +10,13 @@ export default function handler(req, res) {
   } else {
     // return error for invalid route
     res.status(400).json({
-    success: "false",
-    error: "The requested resource does not exist",
-    message:"Make sure you are sending a request to a valid endpoint or web-page",
-    info: {
-      credits: "Made by harys722, available only for cool people.",
-      support: "https://harys.is-a.dev/api"
-    }
-  });
+      success: "false",
+      error: "The requested resource does not exist",
+      message: "Make sure you are sending a request to a valid endpoint or web-page",
+      info: {
+        credits: "Made by harys722, available only for cool people.",
+        support: "https://harys.is-a.dev/api"
+      }
+    });
+  }
 }
